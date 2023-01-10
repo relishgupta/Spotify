d407250bb12c67d1cba852843f558a4976c0d211
Relish Gupta <119758940+relishgupta@users.noreply.github.com>
2023-01-10 16:53:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+//Initialize the variables
+let songIndex=0;
+let audioElement=new Audio('1.mp3');
+let masterPlay=document.getElementById('masterPlay');
+let myProgressBar=document.getElementById('myProgressBar');
+let gif=document.getElementById('gif');
+let previous=document.getElementById('previous');
+let next=document.getElementById('next');
+let masterSongName=document.getElementById('masterSongName');
+
+let songs=[
+    {songName:"Bt Ho Gayi - Paradox", filePath:"1.mp3", coverPath:"cover1.jpg"},
+    {songName:"Jaadugar - Paradox", filePath:"2.mp3", coverPath:"cover1.jpg"},
+    {songName:"Gang wale munde - Paradox", filePath:"3.mp3", coverPath:"cover1.jpg"},
+    {songName:"Glitch - Paradox", filePath:"4.mp3", coverPath:"cover1.jpg"},
+    {songName:"Mileya ni - Paradox", filePath:"5.mp3", coverPath:"cover1.jpg"},
+]
+//Handle play/pause click
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterSongName.innerText=songs[songIndex].songName;
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity=1;
+    }
+    else{
+        audioElement.pause();
+        masterSongName.innerText="";
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
+        gif.style.opacity=0;
+    }
+})
+
+// Listen to Events
+audioElement.addEventListener('timeupdate',()=>{
+    //Update Seekbar
+    progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value=progress;
+})
+
+myProgressBar.addEventListener('change',()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays=()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-circle-pause');
+        element.classList.add('fa-circle-play');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click',(e)=>{
+        makeAllPlays();
+        songIndex=parseInt(e.target.id);
+        if(audioElement.paused || audioElement.currentTime<=0){
+            e.target.classList.remove('fa-circle-play');
+            e.target.classList.add('fa-circle-pause');
+            audioElement.src=`${songIndex+1}.mp3`;
+            masterSongName.innerText=songs[songIndex].songName;
+            audioElement.currentTime=0;
+            audioElement.play();
+            gif.style.opacity=1;
+            masterPlay.classList.remove('fa-circle-play');
+            masterPlay.classList.add('fa-circle-pause');
+        }
+        else{
+            e.target.classList.remove('fa-circle-pause');
+            e.target.classList.add('fa-circle-play');
+            masterSongName.innerText="";
+            audioElement.pause();
+            gif.style.opacity=0;
+            masterPlay.classList.remove('fa-circle-pause');
+            masterPlay.classList.add('fa-circle-play');
+        }
+    })
+})
+
+previous.addEventListener('click',()=>{
+    if(songIndex<=0){
+        songIndex=4;
+    }
+    else{
+        songIndex-=1;
+    }
+    audioElement.src=`${songIndex+1}.mp3`;
+    masterSongName.innerText=songs[songIndex].songName;
+    audioElement.currentTime=0;
+    audioElement.play();
+    gif.style.opacity=1;
+    masterPlay.classList.remove('fa-circle-play');
+    masterPlay.classList.add('fa-circle-pause');
+})
+
+next.addEventListener('click',()=>{
+    if(songIndex>=4){
+        songIndex=0;
+    }
+    else{
+        songIndex+=1;
+    }
+    audioElement.src=`${songIndex+1}.mp3`;
+    masterSongName.innerText=songs[songIndex].songName;
+    audioElement.currentTime=0;
+    audioElement.play();
+    gif.style.opacity=1;
+    masterPlay.classList.remove('fa-circle-play');
+    masterPlay.classList.add('fa-circle-pause');
+})
